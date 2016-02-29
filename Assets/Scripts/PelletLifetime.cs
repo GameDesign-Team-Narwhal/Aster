0f6eafe0624f1e3d9a6f63dfdc8cb0293b12b4aa
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-02-29 13:16:30 -0800

    Add pellet

---
+++ Assets/Scripts/PelletLifetime.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PelletLifetime : MonoBehaviour
+{
+	public Color startingColor, endingColor;
+
+	public float lifetimeSec;
+
+	SpriteRenderer spriteRenderer;
+	float startingTime;
+
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer.color = startingColor;	
+		startingTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float timeSinceSpawn = Time.time - startingTime;
+
+		spriteRenderer.color = Color.Lerp (startingColor, endingColor, timeSinceSpawn / lifetimeSec);
+
+		if (timeSinceSpawn > lifetimeSec) {
+			GameObject.Destroy(gameObject);
+		}
+	
+	}
+}
