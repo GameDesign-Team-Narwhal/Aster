a973892bb1922f25a6a8a42b25d179b1b0966aa6
jeff Hufflpuff <tehcrazyalex@gmail.com>
2016-02-29 13:19:30 -0800

    Added Asteroids
    MORE ASTEROIDS

---
+++ Assets/Scripts/AstroidMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroidMovement : MonoBehaviour {
+
+ Rigidbody2D body2d;
+	public float AsteroidSpeed = 1f;
+	public float SpeedOffset = 1f;
+
+	void Awake ()
+	{
+		body2d = GetComponent<Rigidbody2D>();
+	}
+	void Start () {
+		var Angle = Random.Range (0, 360);
+		body2d.velocity = Utils.VecFromAngleMagnitude (Angle, AsteroidSpeed + Random.Range (-SpeedOffset, SpeedOffset));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
