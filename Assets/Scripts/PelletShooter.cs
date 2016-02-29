37e6348d8a125a253436baaebc1b0b0a2054f4ef
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-02-29 12:33:58 -0800

    Work on pellet shooter script

---
+++ Assets/Scripts/PelletShooter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PelletShooter : MonoBehaviour {
+
+	public GameObject pelletPrefab;
+
+	public float pelletSpeed;
+
+	Rigidbody2D body2D;
+
+	void Awake()
+	{
+		body2D = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space))
+		{
+			GameObject pellet = GameObject.Instantiate(pelletPrefab);
+
+			Rigidbody2D pelletBody = pellet.GetComponent<Rigidbody2D>();
+			
+		}
+	}
+}
