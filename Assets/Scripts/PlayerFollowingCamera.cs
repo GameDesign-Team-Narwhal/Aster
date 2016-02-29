82698c2c48584638a2a7a854370f3c9bd6410ae9
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-02-29 13:23:16 -0800

    Add non-sickening camera

---
+++ Assets/Scripts/PlayerFollowingCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollowingCamera : MonoBehaviour {
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = player.transform.position;
+		newPosition.z = -10;
+		transform.position = newPosition;
+	}
+}
