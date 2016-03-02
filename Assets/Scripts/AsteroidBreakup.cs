3511968cd383615d530cdd415ba508ebc2babc5e
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-03-02 12:19:50 -0800

    Make asteroids destructable

--- Assets/Scripts/AsteroidBreakup.cs
+++ Assets/Scripts/AsteroidBreakup.cs
@@ -4,14 +4,41 @@ using System;
 
 public class AsteroidBreakup : MonoBehaviour, IShootable
 {
+    Animator breakupAnim;
+
+    bool alreadyDead;
+
     public void OnShotBy(GameObject shooter)
     {
-        GameObjectUtil.Destroy(gameObject);
+        if (!alreadyDead)
+        {
+            StartCoroutine(BreakupCoroutine());
+
+            alreadyDead = true;
+        }
+    }
+
+    void Awake()
+    {
+        breakupAnim = GetComponent<Animator>();
     }
 
 
     // Update is called once per frame
     void Update () {
-	
 	}
+    
+
+    IEnumerator BreakupCoroutine()
+    {
+        breakupAnim.SetTrigger("Breakup");
+        yield return null;
+
+        Debug.Log("Anim length:" + breakupAnim.GetCurrentAnimatorClipInfo(0).Length / 2.0);
+
+        yield return new WaitForSeconds((breakupAnim.GetCurrentAnimatorClipInfo(0).Length) / 2f);
+
+
+        GameObject.Destroy(gameObject);
+    }
 }
