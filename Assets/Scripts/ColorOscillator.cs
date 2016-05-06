11340a434735cd626d2b551470063fab9305f7d4
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2016-05-06 13:16:36 -0700

    Add color oscillator script

---
+++ Assets/Scripts/ColorOscillator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorOscillator : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+
+	public float saturation = 1, lightness = .5, alpha = 1;
+	public float currentH = 0;
+
+	public float huePerSecond = 1;
+
+	// Use this for initialization
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentH += huePerSecond * Time.deltaTime;
+
+		if(currentH > 1)
+		{
+			currentH -= 1;
+		}
+		Color newColor = Utils.ColorFromHSL(currentH, saturation, lightness);
+		newColor.a = alpha;
+		//Debug.Log("New color:" + newColor.ToString());
+
+		spriteRenderer.color = newColor;
+	}
+}
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -48,5 +48,45 @@ public class Utils
     {
         return Mathf.Rad2Deg * Mathf.Atan2(vector.y, vector.x);
     }
+
+	/**
+	 * From http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+	 * 
+	 * Converts an HSL color value to RGB. Conversion formula
+	 * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+	 * Assumes h, s, and l are contained in the set [0, 1] and
+	 * returns r, g, and b in the set [0, 255].
+	 *
+	 * @param h       The hue
+	 * @param s       The saturation
+	 * @param l       The lightness
+	 * @return Color  The RGB representation
+	 */
+	public static Color ColorFromHSL(float h, float s, float l){
+		float r = 0, g = 0, b = 0;
+		
+		if(s == 0){
+			r = g = b = l; // achromatic
+		}else{
+
+			
+			float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			float p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1/3f);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3f);
+		}
+		
+		return new Color(r, g, b);
+	}
+
+	private static float hue2rgb(float p, float q, float t){
+		if(t < 0) t += 1;
+		if(t > 1) t -= 1;
+		if(t < 1/6f) return p + (q - p) * 6 * t;
+		if(t < 1/2f) return q;
+		if(t < 2/3f) return p + (q - p) * (2/3f - t) * 6;
+		return p;
+	}
 }
 
