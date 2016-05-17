ff8d3716625cfe8c2be46e2a1f2eb55de4c4173c
Alex Lillian <tehcrazyalex@gmail.com>
2016-05-17 12:30:24 -0700

    Destroyer Fixed

--- Assets/Scripts/FireRateUp.cs
+++ Assets/Scripts/FireRateUp.cs
@@ -6,19 +6,19 @@ public class FireRateUp : MonoBehaviour {
 	// Use this for initialization
 	void Awake ()
 	{
-		Debug.Log("Fire Rate up");
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("Fire Rate up");
+
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log("Fire Rate up");
+
 		if(other.gameObject.Equals(GameController.instance.playerShipInstance))
 		{
-			Debug.Log("Fire Rate up");
+
 			GameController.instance.PlayerCoolDownLower(CoolDownLower);
 			GameObject.Destroy(gameObject);
 		}
