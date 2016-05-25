b554022aca6d0f51388a7eccf0132397e4ef107c
Alex Lillian <tehcrazyalex@gmail.com>
2016-05-25 12:57:17 -0700

    fixing things

---
+++ Assets/Scripts/EngineUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineUp :  Upgrade
+{
+	public float dragUp = .1f;
+	public int TorqueUp = 100;
+	private Rigidbody2D body2d;
+	
+	protected override void BuffPlayer(GameObject player)
+	{
+		body2d = GetComponent<Rigidbody2D>();
+		body2d.angularDrag += dragUp;
+		player.GetComponent<PlayerControlledShip> ().turningTorque += TorqueUp;
+	}
+	
+	protected override string GetMessage()
+	{
+		return "Handleding Up!";
+	}
+}
